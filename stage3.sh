b64ad0bd7bf837ee379fa4289039df6692f46831
JimBouse <jim@mobileitpro.com>
2024-01-11 17:12:24 -0600

    Update stage3.sh

--- stage3.sh
+++ stage3.sh
@@ -38,8 +38,8 @@ sed -i '/CONFIG_DB_PASS/d' /var/www/html/daloradius/library/daloradius.conf.php
 
 
 wget -O /var/www/html/webhook.php https://raw.githubusercontent.com/jimbouse/ucrm-freeradius-auth/master/php_files/webhook.php
-wget -O /var/www/html/webhook.php https://raw.githubusercontent.com/jimbouse/ucrm-freeradius-auth/master/php_files/functions.php
-wget -O /var/www/html/webhook.php https://raw.githubusercontent.com/jimbouse/ucrm-freeradius-auth/master/php_files/service.edit.php
+wget -O /var/www/html/functions.php https://raw.githubusercontent.com/jimbouse/ucrm-freeradius-auth/master/php_files/functions.php
+wget -O /var/www/html/service.edit.php https://raw.githubusercontent.com/jimbouse/ucrm-freeradius-auth/master/php_files/service.edit.php
 
 sudo chown -R www-data:www-data /var/www/html/
 
