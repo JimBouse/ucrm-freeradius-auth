1a6f5ada9271977a5ee307d6ecfc6a2402502e1f
JimBouse <jim@mobileitpro.com>
2024-05-10 12:41:34 -0500

    Update add_sql_trigger.sql

--- add_sql_trigger.sql
+++ add_sql_trigger.sql
@@ -1,11 +1,11 @@
 DROP TRIGGER IF EXISTS  `add_unknown_devices`;
 DELIMITER //
-CREATE TRIGGER `add_unknown_devices` AFTER UPDATE ON `radpostauth` 
+CREATE TRIGGER `add_unknown_devices` AFTER INSERT ON `radpostauth` 
 FOR EACH ROW 
 BEGIN
    IF NEW.reply = 'Access-Reject' THEN
      INSERT INTO radcheck (username, attribute, op, value) (SELECT NEW.username, 'Auth-type', ':=', 'Accept' WHERE NOT EXISTS(SELECT username FROM radcheck WHERE username = NEW.username));
-     INSERT INTO radusergroup(username, groupname, priority) VALUES (OLD.username, 'Service_Unknown_Device', 1);
+     INSERT INTO radusergroup(username, groupname, priority) VALUES (NEW.username, 'Service_Unknown_Device', 1);
    END IF;
 END//
 DELIMITER ;
