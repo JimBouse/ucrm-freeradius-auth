b1b9b09f44efe0f8542751295940d5081f3e6593
Jim Bouse <jim@mobileitpro.com>
2024-08-09 16:24:36 -0500

    Update add_queue_name.sql

--- sql/add_queue_name.sql
+++ sql/add_queue_name.sql
@@ -1,50 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1deb3
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Aug 09, 2024 at 03:49 PM
--- Server version: 10.11.6-MariaDB-2
--- PHP Version: 8.2.12-1ubuntu2
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `radius`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `queue_name`
---
-
 CREATE TABLE `queue_name` (
   `wanIP` varchar(100) NOT NULL,
   `www_port` int(11) NOT NULL,
-  `mac` varchar(12) NOT NULL,
+  `mac` varchar(17) NOT NULL,
   `queue` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='This table contains a reference to the queue name created by the Mikrotik router upon DHCP lease.';
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `queue_name`
---
 ALTER TABLE `queue_name`
   ADD PRIMARY KEY (`mac`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
