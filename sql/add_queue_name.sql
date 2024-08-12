a2fbdc3736076cf6ca999fbed357865d8c7b323c
Jim Bouse <jim@mobileitpro.com>
2024-08-12 08:42:37 -0500

    Update add_queue_name.sql

--- sql/add_queue_name.sql
+++ sql/add_queue_name.sql
@@ -2,7 +2,8 @@ CREATE TABLE `queue_name` (
   `wanIP` varchar(100) NOT NULL,
   `www_port` int(11) NOT NULL,
   `mac` varchar(17) NOT NULL,
-  `queue` varchar(100) NOT NULL
+  `queue` varchar(100) NOT NULL,
+  `ip` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='This table contains a reference to the queue name created by the Mikrotik router upon DHCP lease.';
 
 
