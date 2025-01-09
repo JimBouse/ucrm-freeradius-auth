7448183db9387090bb78701cf29fb0134a316b37
Jim Bouse <jim@mobileitpro.com>
2025-01-09 08:01:17 -0600

    Update and rename getQueueInfo.php to getInfo.php

--- php_files/getInfo.php
+++ php_files/getInfo.php
@@ -12,5 +12,12 @@ if (isset($_GET['mac'])) {
 	while ($row = mysqli_fetch_assoc($result)) {
 		print json_encode($row);
 	}
+} elseif (isset($_GET['ip'])) {
+        $sql = "SELECT * FROM queue_name WHERE ip = '".mysqli_real_escape_string($link, $_GET['ip'])."' LIMIT 1";
+        $result = mysqli_query($link,$sql) or trigger_error("Query Failed! SQL: $sql - Error: ".mysqli_error(), E_USER_ERROR);
+        while ($row = mysqli_fetch_assoc($result)) {
+                print json_encode($row);
+        }
 }
+
 ?>
